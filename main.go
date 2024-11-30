40752940a82b34ee657837c14d7db617f7e4eb4a
undesicimo <105863111+undesicimo@users.noreply.github.com>
2024-12-01 00:22:26 +0900

    initial

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/go-rod/rod"
+)
 
 func main() {
-    fmt.Println("Hello, Go!")
+	// Login to Rakuten Card
+	browser := rod.New().MustConnect()
+	defer browser.MustClose()
+	RakutenPage := browser.MustPage("https://login.account.rakuten.com/sso/authorize?client_id=rakuten_card_enavi_web&redirect_uri=https://www.rakuten-card.co.jp/e-navi/auth/login.xhtml&scope=openid%20profile&response_type=code&prompt=login#/sign_in").MustWaitStable()
+	fmt.Println("Login Page loaded, Inputing User ID")
+	RakutenPage.MustElement("input[id='user_id']").MustInput("dehaanchillax13")
+
+	fmt.Println("going next")
+	RakutenPage.MustElementX("//*[@id='cta001']").MustClick()
+	fmt.Println("Password Inputted, going next")
+	RakutenPage.MustElement("input[id='password_current']").MustInput("Vongola26//")
+	fmt.Println("Inputting password again")
+	RakutenPage.MustElementX("//*[@id='cta011']").MustClick()
+	RakutenPage.MustWaitStable().MustElement("input[id='password_current']").MustInput("Vongola26//")
+	RakutenPage.MustElementX("//*[@id='cta011']").MustClick()
+	fmt.Println("Logged in")
+	RakutenPage.MustWaitStable().MustElement("select[id='cardChangeForm:cardtype']").MustSelect("楽天カード（Visa）")
+	fmt.Println("Getting the amount")
+	amount, e := RakutenPage.MustWaitStable().MustElementX("//*[@id='js-rd-billInfo-amount_show']/span").Text()
+	if e != nil {
+		fmt.Println(e)
+	}
+	fmt.Println(amount)
 }
